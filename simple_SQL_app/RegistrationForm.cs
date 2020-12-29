500a9397fc74ba76fb33749555414af4bbf0bb87
victor_shulpov <shulpovvitya01@mail.ru>
2020-12-30 03:37:43 +0700

    регистрация

--- simple_SQL_app/RegistrationForm.cs
+++ simple_SQL_app/RegistrationForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -27,6 +28,32 @@ namespace simple_SQL_app
             LoginForm loginForm = new LoginForm();
             loginForm.Show();
             this.Hide();
+        }
+
+        private void RegisterButton_Click(object sender, EventArgs e)
+        {
+            //содание БД
+            DataBase data_base = new DataBase();
+            string sql_command = "INSERT INTO `users` (`login`, `password`, `name`, `surname`) VALUES(@login, @password, @name, @surname);";
+            MySqlCommand command = new MySqlCommand(sql_command, data_base.GetConnection());
+            //переменые-заглушки заполняются значениями из полей формы
+            command.Parameters.Add("@login", MySqlDbType.VarChar).Value = LoginTextBox.Text;
+            command.Parameters.Add("@password", MySqlDbType.VarChar).Value = PasswordTextBox.Text;
+            command.Parameters.Add("@name", MySqlDbType.VarChar).Value = NameTextBox.Text;
+            command.Parameters.Add("@surname", MySqlDbType.VarChar).Value = SurnameTextBox.Text;
+
+            data_base.openConnection();
+              try {
+                if (command.ExecuteNonQuery() == 1)
+                    MessageBox.Show("Регистрация прошла успешно!");
+                else
+                    MessageBox.Show("Регистрация прервана.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Регистрация прервана.\n" + ex.Message);
+            }
+            data_base.closeConnection();
         }
     }
 }
