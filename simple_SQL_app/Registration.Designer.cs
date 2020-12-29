86f2edc3ce091980c1ffc12dcc05246cf640c128
victor_shulpov <shulpovvitya01@mail.ru>
2020-12-30 02:06:00 +0700

    форма регистрации

---
+++ simple_SQL_app/Registration.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace simple_SQL_app
+{
+    partial class Registration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.LoginLabel = new System.Windows.Forms.Label();
+            this.LoginTextBox = new System.Windows.Forms.TextBox();
+            this.RegistrationLabel = new System.Windows.Forms.Label();
+            this.RegisterButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.AutoSize = true;
+            this.PasswordLabel.Location = new System.Drawing.Point(274, 199);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(48, 13);
+            this.PasswordLabel.TabIndex = 11;
+            this.PasswordLabel.Text = "Пароль:";
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(277, 215);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.PasswordChar = '*';
+            this.PasswordTextBox.Size = new System.Drawing.Size(255, 20);
+            this.PasswordTextBox.TabIndex = 10;
+            this.PasswordTextBox.UseSystemPasswordChar = true;
+            // 
+            // LoginLabel
+            // 
+            this.LoginLabel.AutoSize = true;
+            this.LoginLabel.Location = new System.Drawing.Point(274, 145);
+            this.LoginLabel.Name = "LoginLabel";
+            this.LoginLabel.Size = new System.Drawing.Size(41, 13);
+            this.LoginLabel.TabIndex = 9;
+            this.LoginLabel.Text = "Логин:";
+            // 
+            // LoginTextBox
+            // 
+            this.LoginTextBox.Location = new System.Drawing.Point(277, 161);
+            this.LoginTextBox.Name = "LoginTextBox";
+            this.LoginTextBox.Size = new System.Drawing.Size(255, 20);
+            this.LoginTextBox.TabIndex = 8;
+            // 
+            // RegistrationLabel
+            // 
+            this.RegistrationLabel.AutoSize = true;
+            this.RegistrationLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.25F);
+            this.RegistrationLabel.Location = new System.Drawing.Point(233, 29);
+            this.RegistrationLabel.Name = "RegistrationLabel";
+            this.RegistrationLabel.Size = new System.Drawing.Size(327, 59);
+            this.RegistrationLabel.TabIndex = 7;
+            this.RegistrationLabel.Text = "Регистрация";
+            // 
+            // RegisterButton
+            // 
+            this.RegisterButton.Location = new System.Drawing.Point(340, 396);
+            this.RegisterButton.Name = "RegisterButton";
+            this.RegisterButton.Size = new System.Drawing.Size(134, 23);
+            this.RegisterButton.TabIndex = 6;
+            this.RegisterButton.Text = "Зарегистрироваться";
+            this.RegisterButton.UseVisualStyleBackColor = true;
+            // 
+            // Registration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PasswordLabel);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.LoginLabel);
+            this.Controls.Add(this.LoginTextBox);
+            this.Controls.Add(this.RegistrationLabel);
+            this.Controls.Add(this.RegisterButton);
+            this.Name = "Registration";
+            this.Text = "Registration";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label PasswordLabel;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.Label LoginLabel;
+        private System.Windows.Forms.TextBox LoginTextBox;
+        private System.Windows.Forms.Label RegistrationLabel;
+        private System.Windows.Forms.Button RegisterButton;
+    }
+}
